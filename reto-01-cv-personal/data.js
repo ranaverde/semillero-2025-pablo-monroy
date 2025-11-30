2ea3475490905efe07983eee39c7f72289a1bb14
pablomonroy <pablomonroy@gmail.com>
2025-11-30 17:13:40 -0600

    Update CV data and enhance styles; improve personal information, skills, and experience presentation.

--- reto-01-cv-personal/data.js
+++ reto-01-cv-personal/data.js
@@ -3,7 +3,7 @@
 const cvData = {
     personal: {
         nombre: "Pablo Monroy",
-        puesto: "Ingeniero de Datos, Desarrollador Snowflake y SQL Server, Desarrollador ASP. Net",
+        puesto: "Ingeniero de Datos, Desarrollador Snowflake, SQL Server, Desarrollador Python y ASP. Net",
         ubicacion: "Monterrey, Nuevo León",
         telefono: "+52812 624 6455",
         email: "pablomonroy@gmail.com",
@@ -12,7 +12,7 @@ const cvData = {
         platzi: "https://platzi.com/p/pablo.monroy/",
         idioma: "Inglés B1 intermedio"
     },
-    perfil: "Especialista en Business Intelligence, Data Engineering y Data Analysis, con más de 25 años de experiencia en diversas áreas de TI, incluyendo desarrollo de sistemas, bases de datos, Data Warehouse y Data Marts[cite: 9]. Mi trayectoria me ha permitido consolidar una profunda experiencia en el ciclo de vida completo de los datos, desde la integración hasta la visualización[cite: 10]. Poseo experiencia en SQL y en procesos ETL[cite: 11]. Actualmente, me especializo como Desarrollador Snowflake, con experiencia en arquitectura de Data Warehouse, complementado con visualización en Power BI[cite: 11]. He liderado la creación de Data Warehouse clave, como el de órdenes de compra y requisiciones de compra[cite: 12]. Mi perfil técnico se complementa con experiencia en programación Python (Jupyter, Pandas, Matplotlib, Seaborn), así como en ASP.Net, HTML, CSS, JavaScript y MVC[cite: 13]. Tengo conocimientos básicos en la nube de Microsoft Azure (SQL Azure, Data Factory, servidores virtuales, aplicaciones web) y AWS, lo que me permite trabajar en entornos híbridos y escalables[cite: 14]. Además de mis habilidades técnicas, cuento con una probada capacidad en consultoría y capacitación, con experiencia en el contacto directo con clientes nacionales e internacionales[cite: 15]. Estoy siempre en constante actualización, tomando cursos de diversas herramientas de software[cite: 16].",
+    perfil: "Especialista en Business Intelligence, Data Engineering y Data Analysis, con más de 25 años de experiencia en diversas áreas de TI, incluyendo desarrollo de sistemas, bases de datos, Data Warehouse y Data Marts. Mi trayectoria me ha permitido consolidar una profunda experiencia en el ciclo de vida completo de los datos, desde la integración hasta la visualización. Poseo experiencia en SQL y en procesos ETL. Actualmente, me especializo como Desarrollador Snowflake, con experiencia en arquitectura de Data Warehouse, complementado con visualización en Power BI. He liderado la creación de Data Warehouse clave, como el de órdenes de compra y requisiciones de compra. Mi perfil técnico se complementa con experiencia en programación Python (Jupyter, Pandas, Matplotlib, Seaborn), así como en ASP.Net, HTML, CSS, JavaScript y MVC. Tengo conocimientos básicos en la nube de Microsoft Azure (SQL Azure, Data Factory, servidores virtuales, aplicaciones web) y AWS, lo que me permite trabajar en entornos híbridos y escalables. Además de mis habilidades técnicas, cuento con una probada capacidad en consultoría y capacitación, con experiencia en el contacto directo con clientes nacionales e internacionales. Estoy siempre en constante actualización, tomando cursos de diversas herramientas de software.",
     
     tecnologias: [
         { nombre: "SQL Server/Azure", anos: "25/10" },
@@ -29,121 +29,121 @@ const cvData = {
     ],
 
     estudios: [
-        { titulo: "Técnico Universitario en Data Science", institucion: "Universidad ONMEX (En curso) [cite: 21]" },
-        { titulo: "Lic. Informática Administrativa", institucion: "UNITEC (TRUNCA) [cite: 20]" }
+        { titulo: "Técnico Universitario en Data Science", institucion: "Universidad ONMEX (En curso)" },
+        { titulo: "Lic. Informática Administrativa", institucion: "UNITEC (TRUNCA)" }
     ],
 
     certificaciones: [
-        "Microsoft® Certified Technology Specialist: SQL Server 2005 [cite: 23]",
-        "Microsoft SQL Server™ 2005 - Implementation and Maintenance [cite: 24]",
-        "Designing and Implementing Data Warehouses With Microsoft SQL Server 7.0 [cite: 26]",
-        "Microsoft Certified Solution Developer: Visual Studio 6.0 [cite: 28]",
-        "Analyzing Requirements and Defining Solution Architectures [cite: 29]",
-        "Designing and Implementing Desktop Applications with Microsoft Visual Basic 6.0 [cite: 31]",
-        "Designing and Implementing Distributed Applications with Microsoft Visual Basic 6.0 [cite: 33]",
-        "Microsoft Certified Professional [cite: 34]",
-        "Microsoft Access 2.0 for Windows™™-Application Development-Exam [cite: 35]",
-        "Microsoft® Word 6.0 for Windows™™ Exam [cite: 37]",
-        "Microsoft® Excel 5.0 for Windows™™ Exam [cite: 38]",
-        "Cadena de cursos de SQL Server Data Warehouse SSIS, SSAS y SSRS en Intersoftware Training[cite: 39].",
-        "Ruta de Business Intelligence y Data Management en Platzi 100% de avance, certificados[cite: 40]."
+        "Microsoft® Certified Technology Specialist: SQL Server 2005",
+        "Microsoft SQL Server™ 2005 - Implementation and Maintenance",
+        "Designing and Implementing Data Warehouses With Microsoft SQL Server 7.0",
+        "Microsoft Certified Solution Developer: Visual Studio 6.0",
+        "Analyzing Requirements and Defining Solution Architectures",
+        "Designing and Implementing Desktop Applications with Microsoft Visual Basic 6.0",
+        "Designing and Implementing Distributed Applications with Microsoft Visual Basic 6.0",
+        "Microsoft Certified Professional",
+        "Microsoft Access 2.0 for Windows™™-Application Development-Exam",
+        "Microsoft® Word 6.0 for Windows™™ Exam",
+        "Microsoft® Excel 5.0 for Windows™™ Exam",
+        "Cadena de cursos de SQL Server Data Warehouse SSIS, SSAS y SSRS en Intersoftware Training.",
+        "Ruta de Business Intelligence y Data Management en Platzi 100% de avance, certificados."
     ],
 
     experiencia: [
         {
-            empresa: "Northware (Asignado a Trayecto) [cite: 42]",
-            puesto: "Ingeniero de Datos [cite: 43]",
-            periodo: "(22/07/2025 - Actual) [cite: 45]",
+            empresa: "Northware (Asignado a Trayecto)",
+            puesto: "Ingeniero de Datos",
+            periodo: "(22/07/2025 - Actual)",
             logros: [
-                "Programar soluciones en C#.NET (MVC, Web, Consola y Windows Forms)[cite: 46].",
-                "Diseñar y administrar estructuras de base de datos relacionales[cite: 46].",
-                "Crear y mantener stored procedures, triggers, funciones, vistas y jobs en SQL Server[cite: 47].",
-                "Integrar y consumir APIs REST para distintas funcionalidades del sistema[cite: 48].",
-                "Gestionar el versionamiento de código usando Git[cite: 49].",
-                "Colaborar en extracción de datos desde Dynamics 365, cuando se requiera[cite: 50].",
-                "Desarrollar, compilar y consumir librerías DLL reutilizables para modularizar la lógica de negocio[cite: 51]."
+                "Programar soluciones en C#.NET (MVC, Web, Consola y Windows Forms).",
+                "Diseñar y administrar estructuras de base de datos relacionales.",
+                "Crear y mantener stored procedures, triggers, funciones, vistas y jobs en SQL Server.",
+                "Integrar y consumir APIs REST para distintas funcionalidades del sistema.",
+                "Gestionar el versionamiento de código usando Git.",
+                "Colaborar en extracción de datos desde Dynamics 365, cuando se requiera.",
+                "Desarrollar, compilar y consumir librerías DLL reutilizables para modularizar la lógica de negocio."
             ]
         },
         {
-            empresa: "D & C Solutions [cite: 52]",
-            puesto: "Arquitecto de Datos [cite: 53]",
-            periodo: "(01/08/2021 - 15/05/2025) [cite: 67]",
+            empresa: "D & C Solutions",
+            puesto: "Arquitecto de Datos",
+            periodo: "(01/08/2021 - 15/05/2025)",
             logros: [
-                "Arquitectura de Data Warehouse en Snowflake[cite: 56].",
-                "Modelado de datos en el Data Lake de Snowflake[cite: 57].",
-                "Tableros en Microsoft Fabric[cite: 57].",
-                "Data Warehouse órdenes de compra colocadas[cite: 58].",
-                "Data Warehouse Inventarios[cite: 58]."
+                "Arquitectura de Data Warehouse en Snowflake.",
+                "Modelado de datos en el Data Lake de Snowflake.",
+                "Tableros en Microsoft Fabric.",
+                "Data Warehouse órdenes de compra colocadas.",
+                "Data Warehouse Inventarios."
             ]
         },
         {
-            empresa: "Investar Financial [cite: 59]",
-            puesto: "Líder Técnico y Consultor BI en el área de Digital Labs [cite: 60]",
-            periodo: "(06/03/2020 - 30/07/2021) [cite: 68]",
+            empresa: "Investar Financial",
+            puesto: "Líder Técnico y Consultor BI en el área de Digital Labs",
+            periodo: "(06/03/2020 - 30/07/2021)",
             logros: [
-                "Procesos ETL con SSIS[cite: 62].",
-                "Programación Python, Programación.Net y Angular (Type Script)[cite: 63].",
-                "Tableros con Power BI[cite: 64].",
-                "Administración PostgreSQL en AWS[cite: 65].",
-                "Administración de Linux Ubuntu en AWS[cite: 66].",
-                "Creación de la aplicación web Infoservices que presentaba Información Estadística de Remesas con Angular, PostgreSQL, Angular y Power BI[cite: 69].",
-                "Creación del Data Warehouse de Remesas[cite: 70]."
+                "Procesos ETL con SSIS.",
+                "Programación Python, Programación.Net y Angular (Type Script).",
+                "Tableros con Power BI.",
+                "Administración PostgreSQL en AWS.",
+                "Administración de Linux Ubuntu en AWS.",
+                "Creación de la aplicación web Infoservices que presentaba Información Estadística de Remesas con Angular, PostgreSQL, Angular y Power BI.",
+                "Creación del Data Warehouse de Remesas."
             ]
         },
         {
-            empresa: "Data IQ [cite: 71]",
-            puesto: "Consultor BI [cite: 72]",
-            periodo: "(01/08/2019 - 06/03/2020) [cite: 75]",
+            empresa: "Data IQ",
+            puesto: "Consultor BI",
+            periodo: "(01/08/2019 - 06/03/2020)",
             logros: [
-                "Reconexión del Data Mart de Estados Financieros[cite: 74]."
+                "Reconexión del Data Mart de Estados Financieros."
             ]
         },
         {
-            empresa: "Formiik [cite: 76]",
-            puesto: "Consultor en Integración e Implementación [cite: 77]",
-            periodo: "(01/12/2013 - 31/07/2019) [cite: 80]",
+            empresa: "Formiik",
+            puesto: "Consultor en Integración e Implementación",
+            periodo: "(01/12/2013 - 31/07/2019)",
             logros: [
-                "Integrar con éxito Formiik con los sistemas de Originación y Cobranza de entidades financieras[cite: 81].",
-                "Capacitación e Integración usando Web Services para trabajar con diversos clientes nacionales e internacionales[cite: 82, 15].",
-                "Desarrollo de un portal de Dictaminación de verificaciones para Fábricas de Francia con.Net y MVC 5[cite: 100].",
-                "Desarrollo del portal de Dictaminación de verificaciones para CMP con.Net y MVC 4[cite: 100].",
-                "Desarrollo de Tiny Formiik, herramienta desktop para el uso de Formiik con C# y Windows Forms[cite: 101].",
-                "Desarrollo de aplicación web 'Originación', consulta de solicitudes generadas en Formiik con MVC3[cite: 102].",
-                "Desarrollo de Tiny Formiik Web, herramienta web para el uso de Formiik con ASP.Net y MVC 5[cite: 102].",
-                "Desarrollo de servicios web para la recepción de solicitudes generadas en Formiik con WCF[cite: 103].",
-                "Desarrollo de tableros para diferentes clientes con Power BI[cite: 104]."
+                "Integrar con éxito Formiik con los sistemas de Originación y Cobranza de entidades financieras.",
+                "Capacitación e Integración usando Web Services para trabajar con diversos clientes nacionales e internacionales.",
+                "Desarrollo de un portal de Dictaminación de verificaciones para Fábricas de Francia con.Net y MVC 5.",
+                "Desarrollo del portal de Dictaminación de verificaciones para CMP con.Net y MVC 4.",
+                "Desarrollo de Tiny Formiik, herramienta desktop para el uso de Formiik con C# y Windows Forms.",
+                "Desarrollo de aplicación web 'Originación', consulta de solicitudes generadas en Formiik con MVC3.",
+                "Desarrollo de Tiny Formiik Web, herramienta web para el uso de Formiik con ASP.Net y MVC 5.",
+                "Desarrollo de servicios web para la recepción de solicitudes generadas en Formiik con WCF.",
+                "Desarrollo de tableros para diferentes clientes con Power BI."
             ]
         },
         {
-            empresa: "Mobiik (antes Emlink) [cite: 105]",
-            puesto: "Especialista en BI y Bases de Datos [cite: 105]",
-            periodo: "(01/11/2007 - 01/11/2013) [cite: 108]",
+            empresa: "Mobiik (antes Emlink)",
+            puesto: "Especialista en BI y Bases de Datos",
+            periodo: "(01/11/2007 - 01/11/2013)",
             logros: [
-                "Consultor del área de BI para los Desarrollos[cite: 106].",
-                "Diseño y optimización de bases de Datos[cite: 106].",
-                "Desarrollo de la sincronización de bases de datos para el producto SalesLink con SSIS, implementación SSRS y su integración SharePoint en Marzam[cite: 111].",
-                "Estrategia e implementación de la migración de bases de Datos para la SEP y PiP hacia SQL Azure[cite: 112].",
-                "Data Mart de Averiguaciones Previas en la PGR[cite: 113].",
-                "Sincronización de bases de datos para Banorte[cite: 114]."
+                "Consultor del área de BI para los Desarrollos.",
+                "Diseño y optimización de bases de Datos.",
+                "Desarrollo de la sincronización de bases de datos para el producto SalesLink con SSIS, implementación SSRS y su integración SharePoint en Marzam.",
+                "Estrategia e implementación de la migración de bases de Datos para la SEP y PiP hacia SQL Azure.",
+                "Data Mart de Averiguaciones Previas en la PGR.",
+                "Sincronización de bases de datos para Banorte."
             ]
         },
         {
-            empresa: "Air-Go Technologies [cite: 115]",
-            puesto: "Líder de proyecto [cite: 116]",
-            periodo: "(1998 - 2007) [cite: 118]",
+            empresa: "Air-Go Technologies",
+            puesto: "Líder de proyecto",
+            periodo: "(1998 - 2007)",
             logros: [
-                "Responsable de la planeación, supervisión y desarrollo de diferentes proyectos[cite: 119].",
-                "Sistema de lectura de huellas digitales[cite: 120].",
-                "Sistema emisor de pólizas en GNP[cite: 121]."
+                "Responsable de la planeación, supervisión y desarrollo de diferentes proyectos.",
+                "Sistema de lectura de huellas digitales.",
+                "Sistema emisor de pólizas en GNP."
             ]
         },
         {
-            empresa: "Tea Educacional y Eres Esser [cite: 122]",
-            puesto: "Instructor [cite: 122]",
-            periodo: "(1992 - 1995) [cite: 125]",
+            empresa: "Tea Educacional y Eres Esser",
+            puesto: "Instructor",
+            periodo: "(1992 - 1995)",
             logros: [
-                "Capacitación a personal de diferentes empresas como Bimbo, Santander[cite: 126].",
-                "Desarrollo de material de capacitación[cite: 127]."
+                "Capacitación a personal de diferentes empresas como Bimbo, Santander.",
+                "Desarrollo de material de capacitación."
             ]
         }
     ]
